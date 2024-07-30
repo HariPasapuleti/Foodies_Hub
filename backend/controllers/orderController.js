fa78b6d3e448a1ab475c9e7b828837db1ed5ca72
Hari Veera Venkat Pasapuleti <haripasapuleti8055@gmail.com>
2024-07-31 01:56:45 +0530

    Created order update status

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // Placing user order from frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "http://localhost:5174";
 
     try {
         const newOrder = new orderModel({
@@ -108,7 +108,20 @@ const listOrders = async (req, res) => {
 
 }
 
+// api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status:req.body.status})
+        res.json({success: true, message: "Status Updated"});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error in updating status"});
+        
+    }
+
+}
 
 
 
-export { placeOrder, verifyOrder, userOrders, listOrders }
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus }
