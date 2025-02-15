c71b965bf8236a0e2c10f45ad9ad5839a836d3cb
Hari Veera Venkat Pasapuleti <haripasapuleti8055@gmail.com>
2025-02-15 17:08:55 +0530

    Refactor PlaceOrder component for improved formatting and consistency in input fields

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
-import "./PlaceOrder.css";
-import { StoreContext } from "../../context/StoreContext";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from 'react'
+import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const PlaceOrder = () => {
   const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext);
@@ -66,117 +66,59 @@ const PlaceOrder = () => {
     }
   };
 
-  return (
-    <form onSubmit={placeOrder} className="place-order">
-      <div className="place-order-left">
-        <p className="title">Delivery Information</p>
 
-        <div className="multi-fields">
-          <input
-            name="firstName"
-            onChange={onChangeHandler}
-            value={data.firstName}
-            type="text"
-            placeholder="First Name"
-            required
-          />
-          <input
-            name="lastName"
-            onChange={onChangeHandler}
-            value={data.lastName}
-            type="text"
-            placeholder="Last Name"
-            required
-          />
+  return (
+    <form onSubmit={placeOrder} className = "place-order">
+
+      <div className="place-order-left">
+        <p className="title"> Delivery Information</p>
+
+        <div className="multi-feilds">
+          <input name='firstName' onChange={ onChangeHandler} value={data.firstName} type="text" placeholder="First Name" required />
+          <input name='lastName' onChange={ onChangeHandler} value={data.lastName} type="text" placeholder="Last Name" required />
         </div>
-        <input
-          name="email"
-          onChange={onChangeHandler}
-          value={data.email}
-          type="email"
-          placeholder="Email address"
-          required
-        />
-        <input
-          name="street"
-          onChange={onChangeHandler}
-          value={data.street}
-          type="text"
-          placeholder="Street"
-          required
-        />
-        <div className="multi-fields">
-          <input
-            name="city"
-            onChange={onChangeHandler}
-            value={data.city}
-            type="text"
-            placeholder="City"
-            required
-          />
-          <input
-            name="state"
-            onChange={onChangeHandler}
-            value={data.state}
-            type="text"
-            placeholder="State"
-            required
-          />
+        <input name='email' onChange={ onChangeHandler} value={data.email} type="email" placeholder='Email address' required/>
+        <input name='street' onChange={ onChangeHandler} value={data.street} type="text" placeholder='Street' required/>
+        <div className="multi-feilds">
+          <input name='city' onChange={ onChangeHandler} value={data.city} type="text" placeholder="City" required />
+          <input name='state' onChange={ onChangeHandler} value={data.state} type="text" placeholder="State" required />
         </div>
-        <div className="multi-fields">
-          <input
-            name="zipcode"
-            onChange={onChangeHandler}
-            value={data.zipcode}
-            type="text"
-            placeholder="Zip code"
-            pattern="^\d{5}$"
-            required
-          />
-          <input
-            name="country"
-            onChange={onChangeHandler}
-            value={data.country}
-            type="text"
-            placeholder="Country"
-            required
-          />
+        <div className="multi-feilds"> required
+          <input name='zipcode' onChange={ onChangeHandler} value={data.zipcode} type="text" placeholder="Zip code" required />
+          <input name='country' onChange={ onChangeHandler} value={data.country} type="text" placeholder="Country" required />
         </div>
-        <input
-          name="phone"
-          onChange={onChangeHandler}
-          value={data.phone}
-          type="text"
-          placeholder="Phone"
-          pattern="^\d{10}$"
-          required
-        />
+          <input name='phone' onChange={ onChangeHandler} value={data.phone} type="text" placeholder='Phone' required/>
       </div>
 
       <div className="place-order-right">
-        <div className="cart-total">
+
+      <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
+
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p> ${ getTotalCartAmount() }</p>
             </div>
             <hr />
             <div className="cart-total-details">
-              <p>Delivery Fee</p>
-              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+              <p>Delevery Fee</p>
+              <p> ${ getTotalCartAmount() === 0 ? 0 : 2 }</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Total</p>
-              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+              <b> ${ getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2 }</b>
             </div>
-          </div>
-          <button type="submit">PROCEED TO PAYMENT</button>
-        </div>
-      </div>
-    </form>
-  );
-};
 
-export default PlaceOrder;
+          </div>
+          <button type='submit' > PROCEED TO PAYMENT </button>
+        </div>
+
+      </div>
+      
+    </form>
+  )
+}
+
+export default PlaceOrder
