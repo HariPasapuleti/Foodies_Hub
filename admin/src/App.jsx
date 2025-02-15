1c6b24facf5e8d6a12b69cbfba5c42a785d6849c
Hari Veera Venkat Pasapuleti <haripasapuleti8055@gmail.com>
2025-02-15 15:43:36 +0530

    `Update route paths to include 'admin' prefix`

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -21,9 +21,9 @@ const App = () => {
       <div className="app-content">
         <Sidebar/>
         <Routes>
-          <Route path = '/add' element = {<Add url = {url}/>}/>
-          <Route path = '/list' element = {<List url = {url}/>}/>
-          <Route path = '/orders' element = {<Orders url = {url}/>}/>
+          <Route path = '/admin/add' element = {<Add url = {url}/>}/>
+          <Route path = '/admin/list' element = {<List url = {url}/>}/>
+          <Route path = '/admin/orders' element = {<Orders url = {url}/>}/>
         </Routes>
       </div>
     </div>
