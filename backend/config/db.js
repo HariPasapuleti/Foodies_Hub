c2ae9e305cde5e1c804414d4f80562572727f5b1
Hari Veera Venkat Pasapuleti <haripasapuleti8055@gmail.com>
2025-02-15 14:44:46 +0530

    Simplify database connection logic by removing unnecessary options from mongoose.connect

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DB Connected");
     } catch (error) {
         console.error("DB Connection Error:", error.message);
